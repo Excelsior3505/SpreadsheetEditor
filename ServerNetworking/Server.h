bf2ae31e330b0fa199fee26b45209cd3fe269ce2
akoumandarakis <kalphabet111@gmail.com>
2017-04-21 19:40:44 -0600

    Server now has functionality for receiving messages with opcodes 0, 8, 9, and accepting usernames.

--- ServerNetworking/Server.h
+++ ServerNetworking/Server.h
@@ -26,7 +26,7 @@ class Server
   std::vector<client_ptr> clients;
   std::queue< std::pair< int, std::string> > received_messages;
   std::vector<int> clientID_toDocID;
-  std::vector<base_ss> spreadsheets;
+  std::vector<base_ss*> spreadsheets;
   int nextID;
 
   Server(boost::asio::io_service& io_service_, const boost::asio::ip::tcp::endpoint& endP);
