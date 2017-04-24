8b20e02a9fb0888ab816222798a94d2081ad9a37
linxil <linxil@lab1-30.eng.utah.edu>
2017-04-24 17:19:07 -0600

    Server when we got client to start working

--- ServerNetworking/dptest.cpp
+++ ServerNetworking/dptest.cpp
@@ -3,7 +3,7 @@ using namespace cs3505;
 int main()
 {
 cs3505::dpgraph x;
-
+/*
 if(x.get_size()!=0){return 1;}
 if(x.has_dependents("s")!=false|x.has_dependees("s")!=false){return 2;}
 x.add_dependency("s","b");
@@ -15,8 +15,8 @@ if(x.add_dependency("b","s")!=1){return 4;}
 x.remove_dependency("s","b");
 if(x.has_dependents("s")!=false|x.has_dependees("b")!=false){return 5;}
 x.add_dependency("s","b");
-x.add_dependency("k","b");
-std::set<std::string> result;
+x.add_dependency("k","b");*/
+std::set<std::string> result;/*
 result=x.get_dependees("b");
 if(result.size()!=2|result.find("s")==result.end()|result.find("k")==result.end()){return 6;}
 result=x.get_dependents("s");
@@ -33,5 +33,7 @@ if(result.size()!=3|result.find("a")==result.end()|result.find("c")==result.end(
 x.replace_dependents("b",newdees);
 result=x.get_dependents("b");
 if(result.size()!=3|result.find("a")==result.end()|result.find("c")==result.end()|result.find("b")==result.end()){return 9;}
-return 0;
+*/
+return x.add_dependency("a","a");
+//return 0;
 }
